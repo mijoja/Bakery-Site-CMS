66498f9eb17ecfc7e0c49203d374fe6b7d47631a
Michael Jansen <jajomi@gmail.com>
2020-02-16 14:20:43 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/mapianas-logo.png'
+import logo from '../img/Mapianas-Logo.png'
 
 const Navbar = class extends React.Component {
   constructor(props) {
