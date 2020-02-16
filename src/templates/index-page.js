95fed4082ae6e8fdddaa7652a26f02ca3685f78c
Michael Jansen <jajomi@gmail.com>
2020-02-16 01:32:39 -0600

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -43,10 +43,7 @@ export const IndexPageTemplate = ({
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+            color: 'black',
             lineHeight: '1',
             padding: '0.25em',
           }}
@@ -56,10 +53,7 @@ export const IndexPageTemplate = ({
         <h3
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
+            color: 'black',
             lineHeight: '1',
             padding: '0.25em',
           }}
